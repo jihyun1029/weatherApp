98498f67181da4d36dba033176923ff0b0ed0c72
jihyun1029 <parkji2028@naver.com>
2025-04-03 15:32:38 +0900

    현재위치 기반 날씨 가져오기

--- src/App.js
+++ src/App.js
@@ -11,15 +11,25 @@ import './App.css';
 function App() {
   const getCurrentLocation = () => {
     console.log("getCurrentLocation");
-    navigator.geolocation.getCurrentPosition((position => {
+    navigator.geolocation.getCurrentPosition((position) => {
       let lat = position.coords.latitude;
       let lon = position.coords.longitude;
-      console.log("현재 위치", lat, lon);
-    }))
+      // console.log("현재 위치", lat, lon);
+      getweatherByCurrentLocation(lat, lon);
+    });
+  };
+
+  const getweatherByCurrentLocation = async (lat, lon) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=API_KEY}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log("data", data);
   }
+
   useEffect(() => {
     getCurrentLocation()
   }, [])
+
   return (
     <div>hiiiiii</div>
   );
