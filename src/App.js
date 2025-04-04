8a05ec5106526005c8ca678be147e534cb0e0ca9
jihyun1029 <parkji2028@naver.com>
2025-04-04 16:06:28 +0900

    로딩 스피너

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WeatherBox from "./component/WeatherBox";
 import WeatherButton from "./component/WeatherButton";
+import ClipLoader from "react-spinners/ClipLoader";
 
 // 1. 앱이 실앵되자마자 현재위치기반의 날씨가 보인다
 // 2. 날씨정보에는 도시, 섭씨 화씨 날씨상태
@@ -15,6 +16,7 @@ function App() {
   const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState('');
+  const [loading, setLoading] = useState(false);
   const cities = ['paris', 'new york', 'tokyo', 'seoul'];
 
   const getCurrentLocation = () => {
@@ -29,18 +31,22 @@ function App() {
 
   const getweatherByCurrentLocation = async (lat, lon) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    setLoading(true);
     let response = await fetch(url);
     let data = await response.json();
     // console.log("data", data);
     setWeather(data);
+    setLoading(false);
   }
 
   const getWeatherByCity = async () => {
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    setLoading(true);
     let response = await fetch(url);
     let data = await response.json();
     // console.log("Data", data);
     setWeather(data);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -53,10 +59,16 @@ function App() {
 
   return (
     <div>
-      <div className="container">
-        <WeatherBox weather={weather} />
-        <WeatherButton cities={cities} setCity={setCity}/>
-      </div>
+      {loading? (
+          <div className="container">
+            <ClipLoader color="#f88c6b" loading={loading} size={150}/>
+          </div>
+        ) : (
+          <div className="container">
+            <WeatherBox weather={weather} />
+            <WeatherButton cities={cities} setCity={setCity}/>
+          </div>
+      )}
     </div>
   );
 }
